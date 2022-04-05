9af6afcc1e097631f513c1b40e355b6554382209
Adindu Maduekwe (maduekwea) <maduekwea@coventry.ac.uk>
2022-04-05 13:43:46 +0100

    my first commit

---
+++ ToDoApp/ToDoApp.Android/Auth/FirebaseAuthentication.cs
@@ -0,0 +1,96 @@
+﻿using Android.Gms.Extensions;
+using Firebase.Auth;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using ToDoApp.Auth;
+using ToDoApp.Models;
+
+namespace ToDoApp.Droid.Auth
+{
+    public class FirebaseAuthentication : IFirebaseAuthentication
+    {
+        public async Task<UserModel> LoginWithEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                var firebaseuser = await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = await firebaseuser.User.GetIdToken(false).AsAsync<GetTokenResult>();
+                var users = new UserModel()
+                {
+                    DisplayName = firebaseuser.User.DisplayName,
+                    Email = firebaseuser.User.Email,
+                    Token = token.Token
+                };
+                return users;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+
+        public async Task<bool> RegisterWithEmailAndPassword(string username, string email, string password)
+        {
+            try
+            {
+                var result = await FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+                var usersProfileBuilder = new UserProfileChangeRequest.Builder();
+                usersProfileBuilder.SetDisplayName(username);
+                await result.User.UpdateProfileAsync(usersProfileBuilder.Build());
+                return result.User != null;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return false;
+            }
+        }
+
+        public async Task ForgetPassword(string email)
+        {
+            try
+            {
+                await FirebaseAuth.Instance.SendPasswordResetEmailAsync(email);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        public string GetUsername()
+        {
+            var users = FirebaseAuth.Instance.CurrentUser;
+            return users?.DisplayName;
+        }
+
+        public string GetUserId()
+        {
+            var users = FirebaseAuth.Instance.CurrentUser;
+            return users?.Uid;
+        }
+
+
+        public bool IsLoggedIn()
+        {
+            var users = FirebaseAuth.Instance.CurrentUser;
+            return users != null;
+        }
+
+        public bool LogOut()
+        {
+            try
+            {
+                FirebaseAuth.Instance.SignOut();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/ViewModels/WelcomePageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Navigation;
+using System.Windows.Input;
+using ToDoApp.Views;
+using Xamarin.Forms;
+
+namespace ToDoApp.ViewModels
+{
+    public class WelcomePageViewModel: BaseViewModel
+    {
+        #region Commands
+
+        public ICommand GetStartedCommand { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public WelcomePageViewModel(
+            INavigationService navigationService) : base(navigationService)
+        { 
+            GetStartedCommand = new Command(GetStartedCommandHandler);
+        }
+
+        #endregion
+
+        #region Command Handlers
+
+        private async void GetStartedCommandHandler()
+        {
+            await _navigationService.NavigateAsync(nameof(AuthPage));
+        }
+
+        #endregion
+    }
+}
